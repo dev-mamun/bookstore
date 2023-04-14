83304b05eebb3bc14e1a15e8c2081dfffde078ff
Md.Abdullah al mamun <mamun1214@gmail.com>
2023-04-14 16:52:44 +0600

    Featch data from api

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -4,21 +4,27 @@
  * Created: 4/12/23
  * Author: Abdullah Al Mamun <mamun1214@gmail.com>
  ****************************************** */
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Remove from './Buttons/Remove';
+import { fetchBooks } from '../redux/books/bookslice';
 
 function BookList() {
   const { books } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
   return (
     <div className="bookList container">
       {books.map((book) => (
-        <div key={book.id}>
+        <div key={book.item_id}>
           <h3>{book.title}</h3>
           <p>{book.author}</p>
           <div className="action-btn">
             <button type="button">Comment</button>
             <button type="button">Edit</button>
-            <Remove bookId={book.id} />
+            <Remove bookId={book.item_id} />
           </div>
         </div>
       ))}

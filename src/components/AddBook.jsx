e1f6382346202ea4429bca719a61ec83c62f6762
Md.Abdullah al mamun <mamun1214@gmail.com>
2023-04-15 13:40:37 +0600

    Apply Style

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -8,6 +8,7 @@ import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import { addNewBook, addBook } from '../redux/books/bookslice';
+import CSS from './Css/Addbook.module.css';
 
 function AddBook() {
   const dispatch = useDispatch();
@@ -35,11 +36,11 @@ function AddBook() {
     }
   };
   return (
-    <div>
-      <h2>Add book</h2>
-      <form onSubmit={submitForm}>
-        <input type="text" placeholder="add book name" value={bookName} onChange={handleBookName} required />
-        <input type="text" placeholder="add author of book" value={author} onChange={handleAuthor} required />
+    <div className={CSS.container}>
+      <h2 className={CSS.h2}>Add book</h2>
+      <form className={CSS.form} onSubmit={submitForm}>
+        <input type="text" placeholder="Book Title" value={bookName} onChange={handleBookName} required />
+        <input type="text" placeholder="Book Author" value={author} onChange={handleAuthor} required />
         <button type="submit">Add book</button>
       </form>
     </div>
--- src/components/Routes/Categories.jsx
+++ src/components/Routes/Categories.jsx
@@ -1,8 +1,17 @@
+/** ****************************************
+ * Project: bookstore
+ * File: Categories.jsx
+ * Created: 4/12/23
+ * Author: Abdullah Al Mamun <mamun1214@gmail.com>
+ ****************************************** */
+import CSS from '../Css/Categories.module.css';
+
 function Categories() {
   return (
-    <div>
-      <button type="button">Status</button>
+    <div className={CSS.container}>
+      <h1>Under Construction</h1>
     </div>
   );
 }
+
 export default Categories;
